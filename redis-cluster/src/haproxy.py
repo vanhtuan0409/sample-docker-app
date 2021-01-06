74384447da85ae393a5b91bde8549be23d2c5bb9
Tuan Vuong <vanhtuan0409@gmail.com>
2021-01-06 18:01:18 +0700

    adding sample envoy

--- redis-cluster/src/haproxy.py
+++ redis-cluster/src/haproxy.py
@@ -2,6 +2,13 @@ from redis import Redis
 import time
 
 r = Redis(host='localhost', port=6379, db=0)
+
+for i in range(1,1000):
+    key = "foo{}".format(i)
+    val = "bar{}".format(i)
+    print("Set key: {}".format(i))
+    r.set(key, val)
+
 r.set('foo', 'bar')
 
 while True:
