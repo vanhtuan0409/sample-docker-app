736b31e53a0c72778d7fbf1a61ffca556c51c1ce
Tuan Vuong <vanhtuan0409@gmail.com>
2021-01-13 10:33:07 +0700

    add script to setup sentinel

--- redis-cluster/repl.sh
+++
@@ -1,3 +0,0 @@
-# Set 6002 as slave of 6001
-# Sentinel do not set this for you
-redis-cli -p 6002 replicaof 127.0.0.1 6001
---
+++ redis-cluster/sentinel_setup.sh
@@ -0,0 +1,7 @@
+# Setup master slave
+redis-cli -p 6002 replicaof 127.0.0.1 6001
+redis-cli -p 6004 replicaof 127.0.0.1 6003
+
+# Sentinel monitor
+redis-cli -p 8001 sentinel monitor mymaster1 127.0.0.1 6001 1
+redis-cli -p 8001 sentinel monitor mymaster2 127.0.0.1 6003 1
