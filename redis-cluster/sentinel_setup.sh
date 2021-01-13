d4e4a87ddf3268d73aaa1ca46c0e372a00765f21
Tuan Vuong <vanhtuan0409@gmail.com>
2021-01-13 18:31:52 +0700

    set timeout

--- redis-cluster/sentinel_setup.sh
+++ redis-cluster/sentinel_setup.sh
@@ -4,4 +4,9 @@ redis-cli -p 6004 replicaof 127.0.0.1 6003
 
 # Sentinel monitor
 redis-cli -p 8001 sentinel monitor mymaster__group1 127.0.0.1 6001 1
+redis-cli -p 8001 sentinel set mymaster__group1 down-after-milliseconds 500
+redis-cli -p 8001 sentinel set mymaster__group1 failover-timeout 500
+
 redis-cli -p 8001 sentinel monitor mymaster__group2 127.0.0.1 6003 1
+redis-cli -p 8001 sentinel set mymaster__group2 down-after-milliseconds 500
+redis-cli -p 8001 sentinel set mymaster__group2 failover-timeout 500
