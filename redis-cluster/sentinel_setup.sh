424bb7c6286488368476e18414324a59d819cf78
Tuan Vuong <vanhtuan0409@gmail.com>
2021-01-13 15:34:33 +0700

    rename group

--- redis-cluster/sentinel_setup.sh
+++ redis-cluster/sentinel_setup.sh
@@ -3,5 +3,5 @@ redis-cli -p 6002 replicaof 127.0.0.1 6001
 redis-cli -p 6004 replicaof 127.0.0.1 6003
 
 # Sentinel monitor
-redis-cli -p 8001 sentinel monitor mymaster_group1 127.0.0.1 6001 1
-redis-cli -p 8001 sentinel monitor mymaster_group2 127.0.0.1 6003 1
+redis-cli -p 8001 sentinel monitor mymaster__group1 127.0.0.1 6001 1
+redis-cli -p 8001 sentinel monitor mymaster__group2 127.0.0.1 6003 1
