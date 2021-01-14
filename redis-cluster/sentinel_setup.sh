36e906949a53217aa3bec7afd2c98da099f2e88a
Tuan Vuong <vanhtuan0409@gmail.com>
2021-01-14 14:32:56 +0700

    update sentinel scripts

--- redis-cluster/sentinel_setup.sh
+++ redis-cluster/sentinel_setup.sh
@@ -3,10 +3,10 @@ redis-cli -p 6002 replicaof 127.0.0.1 6001
 redis-cli -p 6004 replicaof 127.0.0.1 6003
 
 # Sentinel monitor
-redis-cli -p 8001 sentinel monitor mymaster1__group1 127.0.0.1 6001 1
-redis-cli -p 8001 sentinel set mymaster1__group1 down-after-milliseconds 500
-redis-cli -p 8001 sentinel set mymaster1__group1 failover-timeout 500
+redis-cli -p 8001 sentinel monitor mymaster1 127.0.0.1 6001 1
+redis-cli -p 8001 sentinel set mymaster1 down-after-milliseconds 500
+redis-cli -p 8001 sentinel set mymaster1 failover-timeout 1000
 
-redis-cli -p 8001 sentinel monitor mymaster2__group2 127.0.0.1 6003 1
-redis-cli -p 8001 sentinel set mymaster2__group2 down-after-milliseconds 500
-redis-cli -p 8001 sentinel set mymaster2__group2 failover-timeout 500
+redis-cli -p 8001 sentinel monitor mymaster2 127.0.0.1 6003 1
+redis-cli -p 8001 sentinel set mymaster2 down-after-milliseconds 500
+redis-cli -p 8001 sentinel set mymaster2 failover-timeout 1000
