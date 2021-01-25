5f2fb531e1d4457fbeb3b899950792ffac8c594e
Tuan Vuong <vanhtuan0409@gmail.com>
2021-01-25 16:26:06 +0700

    update test script for both get/set

--- redis-cluster/src/haproxy.py
+++ redis-cluster/src/haproxy.py
@@ -3,17 +3,12 @@ import time
 
 r = Redis(host='localhost', port=6379, db=0)
 
-for i in range(1,1000):
-    key = "foo{}".format(i)
-    val = "bar{}".format(i)
-    print("Set key: {}".format(i))
-    r.set(key, val)
-
 while True:
     for i in range(1,1000):
         key = "foo{}".format(i)
         expected_val = "bar{}".format(i)
         try:
+            r.set(key, expected_val)
             val = r.get(key).decode("utf-8")
             if val == expected_val:
                 print("OK key {}".format(key))
