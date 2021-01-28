5bb33fa9ed414bc6fc1ffa442c61844190a5ee09
Tuan Vuong <vanhtuan0409@gmail.com>
2021-01-28 10:31:39 +0700

    add sharded key

---
+++ redis-cluster/src/shard.py
@@ -0,0 +1,42 @@
+from redis import Redis
+import time
+import sys
+
+r = Redis(host='localhost', port=6379, db=0)
+
+def gen_kv(i):
+    return ("key:{}".format(i), "val:{}".format(i))
+
+def read(N):
+    for i in range(N):
+        (key, expected_val) = gen_kv(i)
+        try:
+            val = r.get(key)
+            if val == expected_val:
+                print("Key is ok: {}".format(key))
+            else:
+                print("Key is ok: {}".format(key))
+        except Exception:
+            print("Key is not OK: {}".format(key))
+
+def write(N):
+    for i in range(N):
+        (key, val) = gen_kv(i)
+        try:
+            r.set(key, val)
+            print("Key is ok: {}".format(key))
+        except Exception:
+            print("Key is not OK: {}".format(key))
+
+def main():
+    assert len(sys.argv) >= 2, "Missing command"
+    command = sys.argv[1].lower()
+    assert command == "read" or command == "write", "Invalid command \"{}\"".format(command)
+    N = 200
+    if command == "read":
+        read(N)
+    else:
+        write(N)
+
+if __name__ == "__main__":
+    main()
