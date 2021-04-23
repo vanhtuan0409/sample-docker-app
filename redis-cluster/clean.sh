df3a57a0f11df74faf13bf58ca4a1bf485beeaa9
Tuan Vuong <vanhtuan0409@gmail.com>
2021-04-23 10:57:06 +0700

    add flush data script

--- redis-cluster/clean.sh
+++ redis-cluster/clean.sh
@@ -2,3 +2,8 @@ redis-cli -p 8001 sentinel remove mymaster1
 redis-cli -p 8001 sentinel remove mymaster2
 redis-cli -p 8001 sentinel remove mymaster__group1
 redis-cli -p 8001 sentinel remove mymaster__group2
+
+redis-cli -p 6001 flushall
+redis-cli -p 6002 flushall
+redis-cli -p 6003 flushall
+redis-cli -p 6004 flushall
