676e70fe79b34d46a345601bd209d23ea8fb9bea
Tuan Vuong <vanhtuan0409@gmail.com>
2021-04-23 10:54:53 +0700

    add cleanup script

---
+++ redis-cluster/clean.sh
@@ -0,0 +1,4 @@
+redis-cli -p 8001 sentinel remove mymaster1
+redis-cli -p 8001 sentinel remove mymaster2
+redis-cli -p 8001 sentinel remove mymaster__group1
+redis-cli -p 8001 sentinel remove mymaster__group2
